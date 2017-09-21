b9864de2aed41bd08761ee945d1970bfb8e4c416
Nitesh Rijal <rijal.it@gmail.com>
2017-09-21 16:53:56 +0545

    added blog migrations, working blog with author and categories\

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,21 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Blog, Category, Author
 
-# Register your models here.
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ('title', 'category', 'created_on', 'last_updated_on', 'is_published')
+    search_fields = ('title', 'category__title')
+    list_filter = ('category',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email',)
+    search_fields = ('name', 'email',)
+
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Author, AuthorAdmin)
--- blog/models.py
+++ blog/models.py
@@ -5,8 +5,29 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    # category = models.ForeignKey('Category')
-    # author = models.ForeignKey('Author')
-    body = models.TextField()
+    slug = models.SlugField(max_length=255, unique=True)
+    category = models.ForeignKey('Category')
+    author = models.ForeignKey('Author', blank=True, null=True)
+    body = models.TextField(blank=True)
+    is_published = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
     last_updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.title)
+
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    title = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return str(self.title)
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=200, unique=True)
+
+    def __str__(self):
+        return str(self.name)
